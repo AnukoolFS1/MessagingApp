1f82d307ec3e9280f05f6d9c5bee1e499788673f
Anukool Singh Chauhan <ascmailing0@gmail.com>
2024-12-16 23:03:42 +0530

    implementation of WebSocket on client side

--- frontend/src/dashboard/Chat.tsx
+++ frontend/src/dashboard/Chat.tsx
@@ -1,7 +1,7 @@
 import { AppDispatch, RootState } from "../redux/store"
 import FirstChat from "./FirstChatDialoge"
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { setCurrentMsg, updateMessages } from "../redux/messagesSlice";
 import MsgUi from "./Messageui";
 import { setMessage, initMsg } from "../redux/initiateMessage";
@@ -12,22 +12,45 @@ const Chat = ({ email, FDC, setFDC }: any) => {
     const dispatch = useDispatch<AppDispatch>()
     const messages = useSelector((state: RootState) => state.messages.currentMessages)
     const initiateMessage = useSelector((state: RootState) => state.intMessage)
-
+    const [ws, setWs] = useState<WebSocket | null>(null)
     const chatContainerRef = useRef<HTMLDivElement>(null);
 
     const typeMessage = (e: React.ChangeEvent<HTMLInputElement>) => {
         dispatch(setMessage(e.target.value))
     }
 
+    useEffect(() => {
+        if (chatContainerRef.current) {
+            chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+        }
+    }, [messages]);
+
+    useEffect(() => {
+        const socket = new WebSocket('ws://localhost:5000/')
+
+        socket.onmessage = (event: MessageEvent) => {
+            console.log(event.data)
+            dispatch(updateMessages(event.data))
+        } 
+
+        socket.onerror = (error) => {
+            console.error(error)
+        }
+
+        socket.onclose = () => {
+            console.log(`ws server has closed`)
+        }
+
+        setWs(socket)
+
+        return () => {
+            socket.close()
+        }
+    }, [])
+
     const onSubmit = async (intMsg: initMsg) => {
         try {
-            const response = await axios.post('http://localhost:5000/chatapp', intMsg, {
-                headers: {
-                    "Content-Type": "application/json",
-                    // "X-custom-user": "taquila"
-                }
-            });
-            dispatch(updateMessages(response.data))
+            ws?.send(JSON.stringify(intMsg))
             dispatch(setCurrentMsg(intMsg.receiver))
             setFDC(false)
             dispatch(setMessage(""))
@@ -36,27 +59,6 @@ const Chat = ({ email, FDC, setFDC }: any) => {
             console.log(err.response)
         }
     }
-
-    useEffect(() => {
-        const eventSource = new EventSource(`http://localhost:5000/messages/${email}`);
-        eventSource.onmessage = (event) => {
-            dispatch(updateMessages(JSON.parse(event.data)))
-            console.log(initiateMessage.receiver)
-            dispatch(setCurrentMsg(initiateMessage.receiver))
-        }
-        eventSource.onerror = (error) => {
-            console.log(error)
-        }
-        return () => {
-            eventSource.close()
-        }
-    }, [email])
-
-    useEffect(() => {
-        if (chatContainerRef.current) {
-            chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
-        }
-    }, [messages]);
 
     return (
         <div className="Chats">
